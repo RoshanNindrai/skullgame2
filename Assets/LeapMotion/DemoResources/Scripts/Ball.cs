a39e7d897c2fe962e4345c0dbbda9ae559a8c96c
Roshan Nindrai Senthilnathan <rsenthilnathan@sapient.com>
2016-10-11 10:48:58 -0500

    Initial upload

---
+++ Assets/LeapMotion/DemoResources/Scripts/Ball.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using Leap;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+
+	public float speed = 1.0f;
+	
+	private HandModel GetHand(Collider other)
+	{
+		if (other.transform.parent && other.transform.parent.parent.parent &&
+		    other.transform.parent.parent.GetComponent<HandModel> ())
+			return other.transform.parent.parent.GetComponent<HandModel> ();
+		else
+			return null;
+	}
+	void onTriggerEnter(Collider other)
+	{
+		HandModel handModel = GetHand (other);
+		if (handModel != null) {
+			speed ++;
+			
+		}
+	}
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
